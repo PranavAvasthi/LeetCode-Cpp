7e05687531a83ac25c6af4c4eee295fbb0c2c825
Pranav Avasthi <96632158+PranavAvasthi@users.noreply.github.com>
2022-08-16 19:43:33 +0530

    Time: 11 ms (87.75%), Space: 44.9 MB (45.68%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int sum=0;
+        int real = x;
+        
+        while(x>0) {
+            int rem = x%10;
+            sum = sum*10 + rem;
+            x = x/10;
+        }
+        
+        if(sum == real) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
