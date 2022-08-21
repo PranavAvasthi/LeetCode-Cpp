9c8d22950ee740a6ce713623ce4aadff0771add2
Pranav Avasthi <96632158+PranavAvasthi@users.noreply.github.com>
2022-08-21 18:09:49 +0530

    Time: 150 ms (76.93%), Space: 68 MB (79.40%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n <=2) {
+        if(n<=2) {
             return 0;
         }
         
@@ -8,16 +8,16 @@ class Solution {
         int limit = (int)Math.sqrt(n);
         
         for(int i=2; i<=limit; i++) {
-            if(comp[i] == false) {
+            if(comp[i]==false) {
                 for(int j=i*i; j<n;j+=i) {
-                    comp[j] = true;
+                    comp[j]=true;
                 }
             }
         }
         
         int cnt=0;
         for(int i=2; i<n; i++) {
-            if(comp[i] == false) {
+            if(comp[i]==false) {
                 cnt++;
             }
         }
