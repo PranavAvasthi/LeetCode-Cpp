2a9dee957e2c1451c56b30c91f0ac130859a1a5d
Pranav Avasthi <96632158+PranavAvasthi@users.noreply.github.com>
2022-08-21 18:08:38 +0530

    Time: 192 ms (49.58%), Space: 68.7 MB (40.08%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n <=2) {
+            return 0;
+        }
+        
+        boolean[] comp = new boolean[n]; // true represents composite numbers.
+        int limit = (int)Math.sqrt(n);
+        
+        for(int i=2; i<=limit; i++) {
+            if(comp[i] == false) {
+                for(int j=i*i; j<n;j+=i) {
+                    comp[j] = true;
+                }
+            }
+        }
+        
+        int cnt=0;
+        for(int i=2; i<n; i++) {
+            if(comp[i] == false) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
